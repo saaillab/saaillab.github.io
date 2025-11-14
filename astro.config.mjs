ed4d1ff241dbc245bbfe91d2c6d8333a08157bb0
inyalala <11386133+inyalala@users.noreply.github.com>
2025-11-14 15:49:40 +0300

    New Site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from 'astro/config';
 // Configuration for saaillab.github.io deployment
 export default defineConfig({
   site: 'https://saaillab.github.io',
+  server: {
+    host: true
+  }
 });
